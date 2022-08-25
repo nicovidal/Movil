06dbb756e6326af3c5155471c1021f6cc8082740
Nicolas Vidal <nico.vidal.m@gmail.com>
2022-08-25 14:25:49 -0400

    clase 3 alertas

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -6,10 +7,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alert.page.scss'],
 })
 export class AlertPage implements OnInit {
-
-  constructor() { }
+  handlerMessage = '';
+  roleMessage = '';
+  //se crea el objeto
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
+  }
+  //es un metodo
+  async saludoOk() {
+    const alert = await this.alertController.create({
+      header: 'Saludos',
+      subHeader: 'wena wena',
+      message: 'Nos Vimo',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async confirma() {
+    const alert = await this.alertController.create({
+      header: 'Tiene hambre?',
+      //crear botones dentro de un arreglo para poder listar los botones.
+      buttons: [
+        {
+          //lo que se ve como usuario es el text no el role.
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+            this.handlerMessage = 'Alert canceled';
+          },
+        },
+        {
+          text: 'Si',
+          role: 'confirm',
+          handler: () => {
+            this.handlerMessage = 'Alert confirmed';
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `Dismissed with role: ${role}`;
+  }
+  async input() {
+    const alert = await this.alertController.create({
+      header: 'Please enter your info',
+      buttons: ['OK'],
+      inputs: [
+        {
+          placeholder: 'Name',
+        },
+        {
+          placeholder: 'Nickname (max 8 characters)',
+          attributes: {
+            maxlength: 8,
+          },
+        },
+        {
+          type: 'number',
+          placeholder: 'Edad',
+          min: 1,
+          max: 100,
+        },
+        {
+          type: 'textarea',
+          placeholder: 'A little about yourself',
+        },
+      ],
+    });
+
+    await alert.present();
   }
 
 }
